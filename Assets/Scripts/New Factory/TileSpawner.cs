15603cdbccc607d8682180a5b27850fac702aedd
KazThomas <mr.firebass@gmail.com>
2024-12-28 17:36:29 +0000

    implementing factory pattern

---
+++ Assets/Scripts/New Factory/TileSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Spawner
+{
+    GameObject Spawn(GameObject caller);
+}
+
+public class TileSpawner : MonoBehaviour, Spawner
+{
+    [SerializeField] private GameObject linePrefab;
+    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private int width, height;
+    private int numSpawns = 0;
+    public GameObject Spawn(GameObject caller)
+    {
+        GameObject newTile = Instantiate(linePrefab);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var flip = transform.rotation.eulerAngles; //some lines need to be veritcal/horizontal 
+                flip.z -= 90;
+                if (x == width - 1 && y < height)
+                {
+                    newTile = Instantiate(linePrefab, new Vector3(x, y), Quaternion.identity);
+                    /* if ( x == 3 && y == 0 )
+                    {
+                        return;
+                    } */
+                }
+                else if (y == 0 && x < width - 1)
+                {
+                    newTile = Instantiate(linePrefab, new Vector3(x, y), Quaternion.Euler(-flip));
+                    //need to figure out why the bottom right tile is the way it is
+                }
+                else
+                {
+                    newTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                }
+            }
+        }
+        return newTile;
+    }
+}
